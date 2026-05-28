03a3dbdcc7f0d41e3b9362c978f3e405bf0f1f47
ciro <cirov553@gmail.com>
2026-05-28 16:27:09 -0300

    Agrego las clases

--- eliminar.php
+++ eliminar.php
@@ -1,3 +1,32 @@
+<?php
+// Agregamos las clases
+include_once 'clases/Database.php';
+include_once 'clases/Producto.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$producto = new Producto($db);
+
+// Capturamos el ID y el botón del index
+$id_producto = isset($_GET['id']) ? $_GET['id'] : null;
+
+if ($id_producto) {
+    // Le pasamos el ID de la clase
+    $producto->id = $id_producto;
+    
+    // Ejecutamos eliminar
+    if ($producto->eliminar()) {
+        // Si sale bien, vuelve al index con un aviso de éxito
+        header("Location: index.php?mensaje=eliminado");
+        exit;
+    } else {
+        echo "Error al intentar eliminar el producto.";
+    }
+} else {
+    header("Location: index.php");
+    exit;
+}
+?>
 <div class="modal fade" id="modalEliminar" tabindex="-1">
 
     <div class="modal-dialog">
