9d9b7700b0db1262dcd1838bb19c6ef5f5bde9b6
luciarosales22092007 <lucia.rosales2209@gmail.com>
2026-05-28 16:19:16 -0300

    Agregacion de codigo eliminar.

---
+++ eliminar.php
@@ -0,0 +1,40 @@
+<div class="modal fade" id="modalEliminar" tabindex="-1">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header bg-danger text-white">
+                <h5 class="modal-title">Confirmar eliminación</h5>
+
+                <button
+                    type="button"
+                    class="btn-close btn-close-white"
+                    data-bs-dismiss="modal">
+                </button>
+            </div>
+
+            <div class="modal-body">
+                ¿Seguro que deseas eliminar este libro?
+            </div>
+
+            <div class="modal-footer">
+
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal">
+                    Cancelar
+                </button>
+
+                <a href="eliminar.php?id=1" class="btn btn-danger">
+                    Sí, eliminar
+                </a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
